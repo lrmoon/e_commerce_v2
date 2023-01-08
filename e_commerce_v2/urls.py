dc6eab91451c1bf017fb89514d01483ec4c2abed
lrmoon <larariad@rocketmail.com>
2023-01-08 15:36:02 -0500

    added cart app and models

--- e_commerce_v2/urls.py
+++ e_commerce_v2/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
