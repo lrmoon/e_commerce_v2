df7927abe5f1395d2466202ae4d78185fa591c68
lrmoon <larariad@rocketmail.com>
2023-02-10 19:53:44 -0500

    dashboard and my orders page

--- store/views.py
+++ store/views.py
@@ -45,6 +45,9 @@ def product_detail(request, category_slug, product_slug):
     except Exception as e:
         raise e
 
+    orderproduct = None
+    user_review = None
+
     if request.user.is_authenticated:
         try:
             orderproduct = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
