ffafd7b40d68b668bbe19c58698a1e0b661ddf86
lrmoon <larariad@rocketmail.com>
2025-12-01 23:49:49 -0500

    bookmarking google console for oauth

--- e_commerce_v2/settings.py
+++ e_commerce_v2/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+##preparing for oauth. using googleconsole 
+
 import environ
 env = environ.Env()
 environ.Env.read_env()
